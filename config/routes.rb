ad3a98138fc42be5a3e04e5ef0b10c8e3eb43320
Carlos Patiño <patmac_p@hotmail.com>
2019-09-29 18:29:00 -0500

    divde code in helpers

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root to: 'movies#index'
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end
+
 end
