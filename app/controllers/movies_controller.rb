f453767cab401b9610df3af36bfab620db70744c
Carlos Patiño <patmac_p@hotmail.com>
2019-09-29 17:36:58 -0500

    Generates controllers, and auth

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,35 @@
+class MoviesController < ApplicationController
+    before_action :authenticate_user!
+    def index
+      #@movies = Movie.all
+    end
+
+    def show
+        @movie
+    end
+
+    def create
+        Movie.create(create_movie)
+    end
+    
+    def update
+        @movie.update(update_movie)
+    end
+
+    def destroy
+        @movie.destroy!
+    end
+
+    private
+
+    def set_movie
+        @movie = Movie.find(params[:id])
+    end
+
+    def update_movie
+        params.require(:movies)
+    end
+    def create_movie
+        params.require(:movies)
+    end
+end
